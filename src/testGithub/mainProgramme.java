32acbbe9574b233e9c44d1df451436313b0fb554
Elvil <Elvil@users.noreply.github.com>
2015-05-26 15:54:55 +0200

    3 commit

--- src/testGithub/mainProgramme.java
+++ src/testGithub/mainProgramme.java
@@ -4,7 +4,8 @@ public class mainProgramme {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("bouh");
+			System.out.println("bouhea");
+			System.out.println("bout");
 	}
 
 }
